5324c5c5b18d126a5817e5c1c279888177e8fbff
Slexx <slex-@hotmail.com>
2025-03-28 19:10:18 +1100

    Gave the esp renderer/window its own thread to try and fix the need to have the esp window focused.

--- Source/Classes/EspWindow.cs
+++ Source/Classes/EspWindow.cs
@@ -32,11 +32,9 @@ namespace DMAW_DND
             GLFW.GetMonitorWorkarea(monitor, out int x, out int y, out int width, out int height);
             GLFW.SetWindowMonitor(this.WindowPtr, monitor, 0, 0, width, height, 60);
 
-            // 1. Get the Win32 window handle (HWND) from the GLFW pointer:
-            IntPtr hWnd = GLFW.GetWin32Window(this.WindowPtr);
+            //IntPtr hWnd = GLFW.GetWin32Window(this.WindowPtr);
 
-            // 2. Apply extended window styles:
-            WindowHelper.SetUnfocusedStyles(hWnd);
+            //WindowHelper.SetUnfocusedStyles(hWnd);
         }
 
         public static FVector2 WorldToScreen(FVector3 worldPos, MinimalViewInfo cameraCache, int screenWidth, int screenHeight)
